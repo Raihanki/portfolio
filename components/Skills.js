c8c8fc148a4d27aefec6b23fe7961d9215ba7736
Raihanki <Raihanhori>
2022-01-16 11:06:31 +0700

    check spelling

--- components/Skills.js
+++ components/Skills.js
@@ -1,7 +1,7 @@
 export default function Skills() {
     return (
         <div className="flex flex-col items-center justify-center w-full font-semibold text-gray-200 pt-14">
-            <h1 className='text-3xl text-center uppercase md:text-left'>Here are a few technologies I’ve been learing</h1>
+            <h1 className='text-3xl text-center uppercase md:text-left'>Here are a few technologies I’ve been learning</h1>
             <div className="w-48 my-10 border-2 border-gray-300" />
             <div className='flex flex-wrap justify-center gap-3'>
                 <div className='px-3 py-2 text-gray-900 transition duration-75 bg-yellow-500 border rounded shadow-sm hover:bg-yellow-700'>PHP</div>
